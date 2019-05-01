7cb91d5ea25435ab2625ce7415a7e49dca30f270
alexladines <alexladines@outlook.com>
2019-05-01 13:26:37 -0700

    Finished UI.

---
+++ Cryptex/Cryptex/View Controllers/CryptexViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CryptexViewController.swift
+//  Cryptex
+//
+//  Created by Alex Ladines on 5/1/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import UIKit
+
+class CryptexViewController: UIViewController {
+
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var letterGuesserPickerView: UIPickerView!
+    @IBOutlet weak var unlockButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unlockButtonPressed(_ sender: Any) {
+        
+    }
+    
+
+}
